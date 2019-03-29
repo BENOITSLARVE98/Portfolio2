45b4953c392403f34805d81261af710f1c4051f4
BenoitSlarve98 <benoitslarve98@gmail.com>
2019-03-28 22:28:37 -0400

    Time Tracker (Enter Activity function)

--- SlarveBenoit_TimeTrackerApp/SlarveBenoit_TimeTrackerApp/App.cs
+++ SlarveBenoit_TimeTrackerApp/SlarveBenoit_TimeTrackerApp/App.cs
@@ -62,60 +62,9 @@ namespace SlarveBenoit_TimeTrackerApp
         }
 
         void TrackedData()
-        class App
-        {
-            string cs = @"server=172.16.6.1; userid=bestAdmin; password=password; database=SlarveBenoit_MDV229_Database_201803; port=8889";
-            MySqlConnection conn = null;
-            Menu myMenu = new Menu();
 
-            public App()
-            {
 
-                myMenu = new Menu("Enter Activity", "View Tracked Data", "Run Calculations", "EXIT");
-                myMenu.Title = "Hello Name, What Would You Like To Do Today?";
-                myMenu.Display();
-                Select();
-
-            }
-
-            bool running = true;
-            private void Select()
-            {
-                while (running)
-                {
-                    Console.WriteLine("=============================");
-                    int selection = Utilities.ValidateInt("Make selection:");
-                    switch (selection)
-                    {
-                        case 1:
-                            EnterActivity();
-                            EnterActivitySecond();
-                            EnterActivityThird();
-                            EnterActivityFourth();
-                            Console.WriteLine("Your Activity has been added");
-                            Console.ReadKey();
-                            myMenu.Display();
-                            Select();
-                            break;
-                        case 2:
-                            TrackedData();
-                            break;
-                        case 3:
-                            Calculations();
-                            break;
-                        case 4:
-                            Console.WriteLine("Exiting program...");
-                            running = false;
-                            Console.ReadKey();
-                            Environment.Exit(1);
-                            break;
-                    }
-                    running = false;
-                }
-
-            }
-
-            void TrackedData()
+        void TrackedData()
         {
             Menu trackedDataMenu = new Menu("Select By Date", "Select By Category", "Select By Description", "Back");
             trackedDataMenu.Title = "View Tracked Data:";
@@ -382,4 +331,109 @@ namespace SlarveBenoit_TimeTrackerApp
         }
         
     }
+
+    void EnterActivity()
+    {
+        Console.Clear();
+        Console.WriteLine("Categories");
+        Console.WriteLine("=============================");
+        conn = new MySqlConnection(cs);
+        MySqlDataReader rdr = null;
+        conn.Open();
+
+        string stm = "SELECT * from activity_categories";
+        MySqlCommand cmd = new MySqlCommand(stm, conn);
+        string version = Convert.ToString(cmd.ExecuteScalar());
+
+        rdr = cmd.ExecuteReader();
+        while (rdr.Read())
+        {
+            Console.WriteLine("" + rdr.GetString(1));
+            //Console.ReadKey();
+        }
+        Console.WriteLine("=============================");
+        string Stringselection = Utilities.ValidateString("Type a Category of Activity:");
+
+
+    }
+    void EnterActivitySecond()
+    {
+        Console.Clear();
+        Console.WriteLine("Descriptions");
+        Console.WriteLine("=============================");
+        conn = new MySqlConnection(cs);
+        MySqlDataReader rdo = null;
+        conn.Open();
+
+        string sto = "SELECT * from activity_descriptions";
+        MySqlCommand cmo = new MySqlCommand(sto, conn);
+        string versiono = Convert.ToString(cmo.ExecuteScalar());
+
+        rdo = cmo.ExecuteReader();
+        while (rdo.Read())
+        {
+            Console.WriteLine("" + rdo.GetString(1));
+            //Console.ReadKey();
+        }
+        Console.WriteLine("=============================");
+        string Stringselection1 = Utilities.ValidateString("Type a Description:");
+        if (Stringselection1 == "Church")
+        {
+
+        }
+    }
+    void EnterActivityThird()
+    {
+        Console.Clear();
+        Console.WriteLine("Dates");
+        Console.WriteLine("=============================");
+        conn = new MySqlConnection(cs);
+        MySqlDataReader rdo = null;
+        conn.Open();
+
+        string sto = "SELECT * from tracked_calendar_dates";
+        MySqlCommand cmo = new MySqlCommand(sto, conn);
+        string versiono = Convert.ToString(cmo.ExecuteScalar());
+
+        rdo = cmo.ExecuteReader();
+        while (rdo.Read())
+        {
+            Console.WriteLine("" + rdo.GetString(1));
+            //Console.ReadKey();
+        }
+        Console.WriteLine("=============================");
+        string Stringselection2 = Utilities.ValidateString("Type a Date:");
+        if (Stringselection2 == "")
+        {
+
+        }
+    }
+    void EnterActivityFourth()
+    {
+        Console.Clear();
+        Console.WriteLine("Times");
+        Console.WriteLine("=============================");
+        conn = new MySqlConnection(cs);
+        MySqlDataReader rdo = null;
+        conn.Open();
+
+        string sto = "SELECT * from activity_times";
+        MySqlCommand cmo = new MySqlCommand(sto, conn);
+        string versiono = Convert.ToString(cmo.ExecuteScalar());
+
+        rdo = cmo.ExecuteReader();
+        while (rdo.Read())
+        {
+            Console.WriteLine("" + rdo.GetString(1));
+            //Console.ReadKey();
+        }
+        Console.WriteLine("=============================");
+        string Stringselection3 = Utilities.ValidateString("Type How long you spent on activity:");
+        if (Stringselection3 == "")
+        {
+
+        }
+    }
+
+}
 }
